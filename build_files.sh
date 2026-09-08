dc1d49f304e7a996591f09ab847ed683ead114c3
Neehari09 <neeharisoule@gmail.com>
2026-09-08 20:31:55 +0530

    Add product fixture data and loaddata to build script for Render

--- build_files.sh
+++ build_files.sh
@@ -2,4 +2,6 @@
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
+python manage.py migrate --noinput
+python manage.py loaddata store_fixture.json
 python manage.py collectstatic --noinput
